38329a0e41ca1c50f26d27eb55fc0d12bf543e72
Rubyseher <rubyseher2002@gmail..com>
2025-11-21 11:07:34 +0530

    tailwindcss for box

--- src/frontend/screens/Trial.jsx
+++ src/frontend/screens/Trial.jsx
@@ -6,14 +6,18 @@ export default function Trial() {
   return (
     <div>
       <ResponsiveDrawer />
-      <section className="max-w-750px mx-auto px-6">
-        <h1 className="text-4xl font-semibold leading-tight text-gray-900">
-          Find the <br />
-          perfect outfit
-        </h1>
-        <p className="mt-4 text-lg text-gray-600">Upload a image and well tell you what goes best with it</p>
-      </section>
-      <DragDrop />
+      <main className="">
+        <div className="max-w-3xl mx-auto">
+          <section >
+            <h1 className="text-4xl font-semibold leading-tight text-gray-900">
+              Find the
+              Perfect outfit
+            </h1>
+            <p className="mt-4 text-lg text-gray-600">Upload a image and well tell you what goes best with it</p>
+          </section>
+          <DragDrop />
+        </div>
+      </main>
     </div>
   );
 }
